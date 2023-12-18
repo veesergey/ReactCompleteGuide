aadb06af3497137224097cf01296f08fe8ccf8a6
Vitaliy Sergeychik <veesergey@gmail.com>
2023-12-18 16:09:47 -0500

    Remove activePlayer state and derive it from game turns

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,19 +3,25 @@ import Player from "./components/Player"
 import {useState} from 'react';
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player==="X"){
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex){
     setActivePlayer((curActivePlayer) => {
       return curActivePlayer === "X" ? "O" : "X";
     })
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player==="X"){
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player:currentPlayer}, ...prevTurns];
       return updatedTurns;
     });
@@ -29,7 +35,7 @@ function App() {
           </ol>
           <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
         </div>   
-        <Log/>
+        <Log turns={gameTurns}/>
       </main>
   )
 }
