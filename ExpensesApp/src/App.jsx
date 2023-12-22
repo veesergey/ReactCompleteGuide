ecaa285c4d0cfa84358b1c38f682a06c7979afac
Vitaliy Sergeychik <veesergey@gmail.com>
2023-12-22 13:52:18 -0500

    Removed extra inputgroup component

--- ExpensesApp/src/App.jsx
+++ ExpensesApp/src/App.jsx
@@ -13,10 +13,10 @@ function App() {
     <div>
       <Header title='Investment Calculator'/>
       <div id="user-input" className='input-group'>        
-        <UserInput type='text' id='initial-investment' title='Initial Investment' placeHolder='Enter Initial Investment'/>
-        <UserInput type='number' id='annual-investment' title='Annual Investment' placeHolder='Enter Annual Investment'/>
-        <UserInput type='number' id='expected-return' title='Expected Return' placeHolder='Enter Expected Return'/>
-        <UserInput type='number' id='duration' title='Duration' placeHolder='Enter Duration in months'/> 
+        <UserInput type='number' id='initial-investment' title='Initial Investment ($)' placeHolder='5000'/>
+        <UserInput type='number' id='annual-investment' title='Annual Investment ($)' placeHolder='1000'/>
+        <UserInput type='number' id='expected-return' title='Expected Return (%)' placeHolder='6'/>
+        <UserInput type='number' id='duration' title='Duration (Months)' placeHolder='12'/> 
       </div>      
       <div id="result">
         <table className='table'>
